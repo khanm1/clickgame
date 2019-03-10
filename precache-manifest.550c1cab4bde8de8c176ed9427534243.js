618917cb4c891748ade69d2e1918bbc14f4d2295
mkhan <riponk9519@gamil.com>
2019-03-09 23:41:00 -0600

    Updates

---
+++ precache-manifest.550c1cab4bde8de8c176ed9427534243.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f98500e31e84fb21f0a",
+    "url": "/clickgame/static/js/runtime~main.619d8fb6.js"
+  },
+  {
+    "revision": "8b7f97ec27f20474d951",
+    "url": "/clickgame/static/js/main.55979c4c.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/clickgame/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "8b7f97ec27f20474d951",
+    "url": "/clickgame/static/css/main.46903c67.chunk.css"
+  },
+  {
+    "revision": "5782c36df55b07df5cb3bea17760c240",
+    "url": "/clickgame/index.html"
+  }
+];
